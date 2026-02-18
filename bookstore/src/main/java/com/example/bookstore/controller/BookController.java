1e08d4d5772275573698c298128fb8b84a0aefa4
admin <zheckakoroluk@gmail.com>
2026-02-18 19:42:36 +0200

    Add CustomGlobalExceptionHandler and validation for DTOs

--- bookstore/src/main/java/com/example/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import com.example.bookstore.dto.BookDto;
 import com.example.bookstore.dto.BookSearchParametersDto;
 import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -35,7 +36,7 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto dto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto dto) {
         return bookService.createBook(dto);
     }
 
