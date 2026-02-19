15733c01c3343132f764cea5d236ca2644ad0d1d
admin <zheckakoroluk@gmail.com>
2026-02-19 22:22:34 +0200

    Implemented pagination, sorting, search and swagger

--- bookstore/src/main/java/com/example/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -5,8 +5,9 @@ import com.example.bookstore.dto.BookSearchParametersDto;
 import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.service.BookService;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,11 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookService bookService;
-
-    @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.getAll();
-    }
 
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
@@ -52,7 +48,13 @@ public class BookController {
     }
 
     @GetMapping("/search")
-    public List<BookDto> searchBooks(BookSearchParametersDto params) {
-        return bookService.searchBooks(params);
+    public Page<BookDto> searchBooks(BookSearchParametersDto params,
+                                     Pageable pageable) {
+        return bookService.searchBooks(params, pageable);
+    }
+
+    @GetMapping
+    public Page<BookDto> getAll(Pageable pageable) {
+        return bookService.getAll(pageable);
     }
 }
