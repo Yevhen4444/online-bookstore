6fd53e10fa883ca0f5b421689c0f120fde5c23b3
admin <zheckakoroluk@gmail.com>
2026-02-20 15:34:17 +0200

    Add swagger annotations to BookController

--- bookstore/src/main/java/com/example/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -18,7 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
@@ -26,35 +30,41 @@ public class BookController {
 
     private final BookService bookService;
 
+    @Operation(summary = "Get book by id")
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
+    @Operation(summary = "Create a new book")
     @PostMapping
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto dto) {
         return bookService.createBook(dto);
     }
 
+    @Operation(summary = "Update book by id")
     @PutMapping("/{id}")
     public BookDto updateBook(@PathVariable Long id, @RequestBody @Valid CreateBookRequestDto dto) {
         return bookService.updateBook(id, dto);
     }
 
+    @Operation(summary = "Delete book by id")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
 
+    @Operation(summary = "Search books by parameters with pagination")
     @GetMapping("/search")
     public Page<BookDto> searchBooks(BookSearchParametersDto params,
-                                     Pageable pageable) {
+                                     @ParameterObject Pageable pageable) {
         return bookService.searchBooks(params, pageable);
     }
 
+    @Operation(summary = "Get all books with pagination")
     @GetMapping
-    public Page<BookDto> getAll(Pageable pageable) {
+    public Page<BookDto> getAll(@ParameterObject Pageable pageable) {
         return bookService.getAll(pageable);
     }
 }
